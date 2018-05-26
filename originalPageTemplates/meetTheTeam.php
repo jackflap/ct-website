ff80aca657d2015d3564d7f98374e5f283b03b43
Man Ng <man.ng@fullsixuk.com>
2018-05-26 12:11:17 +0100

    add git ignore and backup templates

---
+++ originalPageTemplates/meetTheTeam.php
@@ -0,0 +1,132 @@
+About Us &gt; Meet the team
+<div class="meet-the-team-page-buffer">
+
+<!--
+<h6 class="history-heading" style="text-align: right; font-size: 8px; font-weight: bold;">About us &gt; Meet the Team</h6>
+//-->
+<div style="padding: 20px 0; display: flex; flex-wrap: wrap;">
+<div style="width: 320px;"><a href="#"><img style="width: 320px; border-radius: 3%;" src="http://comm-tech.org/wp-content/uploads/2017/07/alan.png" /></a></div>
+<div class="colleagues-description" style="text-align: justify; flex: 1;">
+<div class="colleagues">Alan Buchel</div>
+<div class="position">Operations Manager</div>
+
+<hr />
+<p style="max-width: 600px;">Comm-Tech's original co-founder, Alan started programming on the 8-bit <a href="https://en.wikipedia.org/wiki/Apple_Lisa" target="_blank" rel="noopener noreferrer">Apple Lisa</a> and <a href="https://en.wikipedia.org/wiki/ZX81" target="_blank" rel="noopener noreferrer">Spectrum ZX81</a> in the early 1980s. After studying Psychology at the University of KwaZulu-Natal in South Africa he moved to the UK and worked on programming and database development for London corporates including Citibank, Lloyds and <a href="https://www.princes-trust.org.uk/" target="_blank" rel="noopener noreferrer">The Prince's Trust</a>. Seeing the need to improve access to technology and the internet in the non-profit and voluntary sector, Alan and Tarik Ibrahim formed <a>The Community Technology Project</a> to reclaim, refurbish and redistribute discarded corporate IT equipment to community groups and small charities.
+Contact Alan at [sm mailto="alan@comm-tech.org" txt="alan@comm-tech.org"].</p>
+
+</div>
+</div>
+<div style="padding: 20px 0; display: flex; flex-wrap: wrap;">
+<div style="width: 320px;"><a href="#"><img style="width: 320px; border-radius: 3%;" src="http://comm-tech.org/andy_ct.jpg" /></a></div>
+<div class="colleagues-description" style="text-align: justify; flex: 1;">
+<div class="colleagues">Andy Lim</div>
+<div class="position">Chief Windows Engineer</div>
+
+<hr />
+<p style="max-width: 600px;">Andy has been with Comm-Tech since 2005, and is currently our Senior Windows Technology Engineer specialising in Hyper-V, Microsoft Exchange, Microsoft Active Directory and Office365 integration.</p>
+
+</div>
+</div>
+<div style="padding: 20px 0; display: flex; flex-wrap: wrap;">
+<div style="width: 320px;"><a href="#"><img style="width: 320px; border-radius: 3%;" src="http://comm-tech.org/wp-content/uploads/2017/06/marka_DSC02508-288x300.jpg" /></a></div>
+<div class="colleagues-description" style="text-align: justify; flex: 1;">
+<div class="colleagues">Mark Anthony</div>
+<div class="position">Chief Linux Engineer</div>
+
+<hr />
+<p style="max-width: 600px;">Mark come from a programming background, working with us initially on developing software for our refurbished computers, moving into an IT Support role in 2015. Mark is our Chief Linux Engineer and Kopano specialist.</p>
+
+</div>
+</div>
+<div style="padding: 20px 0; display: flex; flex-wrap: wrap;">
+<div style="width: 320px;"><a href="#"><img style="width: 320px; border-radius: 3%;" src="https://comm-tech.org/maria_ct.jpeg" /></a></div>
+<div class="colleagues-description" style="text-align: justify; flex: 1;">
+<div class="colleagues">Maria Sharapova</div>
+<div class="position">IT Support Engineer</div>
+
+<hr />
+<p style="max-width: 600px;">Maria joined Comm-Tech in 2016 with background in web development. She previously worked in the oil industry with a number of Russian corporate and non-government organisations as analyst and consultant. Following her early interest in mathematics and armed with a BSc in Pure Mathematics, she is currently enjoying programming. Her greatest achievements were coordinating the implementation of a framework agreement under which <a href="https://en.wikipedia.org/wiki/TNK-BP" target="_blank" rel="noopener">TNK-BP</a> could access technology specialists outside of Russia, and securing <a href="https://en.wikipedia.org/wiki/Computer_Aid_International" target="_blank" rel="noopener">Computer Aid International</a> with <a href="https://10times.com/vsat-london/speakers" target="_blank" rel="noopener">VSAT Global</a> to raise awareness of the high cost of internet in Africa.
+Her passion is the sharing of technological knowledge enjoyed by the corporates with local community organisations.
+Contact Maria at [sm mailto="maria@comm-tech.org" txt="maria@comm-tech.org"]</p>
+
+</div>
+</div>
+<div style="padding: 20px 0; display: flex; flex-wrap: wrap;">
+<div style="width: 320px;"><a href="#"><img style="width: 320px; border-radius: 3%;" src="http://comm-tech.org/wp-content/uploads/2012/06/vanessa-300x225.jpg" /></a></div>
+<div class="colleagues-description" style="text-align: justify; flex: 1;">
+<div class="colleagues">Vanessa Price-Frankson</div>
+<div class="position">Accounts and Compliance Manager</div>
+
+<hr />
+<p style="max-width: 600px;">Vanessa, a bookkeeper with 20 years of experience, joined Comm-Tech part-time in 2012, and has since overhauled the company's financial recording systems. She also works with a diverse range of other clients, from a children's nursery to a high-end property developer.</p>
+
+</div>
+</div>
+<div style="padding: 20px 0; display: flex; flex-wrap: wrap;">
+<div style="width: 320px;"><a href="#"><img style="width: 320px; border-radius: 3%;" src="http://comm-tech.org/wp-content/uploads/2017/06/jaayjay-282x300.png" /></a></div>
+<div class="colleagues-description" style="text-align: justify; flex: 1;">
+<div class="colleagues">James Joseph</div>
+<div class="position">Technical Adviser</div>
+
+<hr />
+<p style="max-width: 600px;">James joined Comm-Tech in 2011 and spent five years developing our cloud desktops and Kopano systems. He moved on to Amazon in 2016 but still remains a loyal volunteer providing advanced networking support to our engineers.</p>
+
+</div>
+</div>
+<div style="padding: 20px 0; display: flex; flex-wrap: wrap;">
+<div style="width: 320px;"><a href="#"><img style="width: 320px; border-radius: 3%;" src="http://comm-tech.org/wp-content/uploads/2017/06/2015-08-Blue-Heron-138-300x200.jpg" /></a></div>
+<div class="colleagues-description" style="text-align: justify; flex: 1;">
+<div class="colleagues">Mark Gensler</div>
+<div class="position">Trustee and Technical Adviser</div>
+
+<hr />
+<p style="max-width: 600px;">Mark became involved with technology at the tender age of five while working in a Japanese pencil factory.</p>
+After studying mathematics at university, Mark has ten years' experience in the financial software industry as both provider and client. He is currently a self-employed website developer when not hacking for Comm-Tech.
+
+</div>
+</div>
+<div style="padding: 20px 0; display: flex; flex-wrap: wrap;">
+<div style="width: 320px;"><a href="#"><img style="width: 320px; border-radius: 3%;" src="http://comm-tech.org/wp-content/uploads/2011/12/alexderizi-300x279.jpg" /></a></div>
+<div class="colleagues-description" style="text-align: justify; flex: 1;">
+<div class="colleagues">Alex Deriziotis</div>
+<div class="position">Trustee and technical adviser</div>
+
+<hr />
+<p style="max-width: 600px;">After cutting his teeth at Comm-Tech building our early Linux-based server platforms in 2005, Alex moved on to developing software that delivers large-scale web applications and services. He has now returned to us as a valuable trustee and highly experienced volunteer</p>
+
+</div>
+</div>
+<div style="padding: 20px 0; display: flex; flex-wrap: wrap;">
+<div style="width: 320px;"><a href="#"><img style="width: 320px; border-radius: 3%;" src="https://comm-tech.org/wp-content/uploads/2017/07/michael-parker-200x300.jpg" /></a></div>
+<div class="colleagues-description" style="text-align: justify; flex: 1;">
+<div class="colleagues">Michael Parker</div>
+<div class="position">Trustee, company secretary and marketing adviser</div>
+
+<hr />
+<p style="max-width: 600px;">Michael is a journalist with more than ten years’ experience writing news and features for UK newspapers and magazines, formerly Associate Editor at The Big Issue in London, now an editor at The Conversation: a news analysis and opinion website where academics write short pieces for a non-specialist audience, bringing their expertise to bear to better inform the public on the issues of the day. Prior to entering journalism Michael worked in technology as a web designer, engineer and systems administrator, including a stint at Comm-Tech in the early 2000s. He retains an interest in Linux and open source software, digital rights, privacy and surveillance.</p>
+
+</div>
+</div>
+<div style="padding: 20px 0; display: flex; flex-wrap: wrap;">
+<div style="width: 320px;"><a href="#"><img style="width: 320px; border-radius: 3%;" src="http://comm-tech.org/wp-content/uploads/2017/06/denise-294x300.jpg" /></a></div>
+<div class="colleagues-description" style="text-align: justify; flex: 1;">
+<div class="colleagues">Denise Lissenden</div>
+<div class="position">Trustee and adviser</div>
+
+<hr />
+<p style="max-width: 600px;">A former deputy headteacher at a London secondary school, Denise has been an invaluable source of advice and support to Comm-Tech, as a friend, volunteer, and trustee.</p>
+
+</div>
+</div>
+<div style="padding: 20px 0; display: flex; flex-wrap: wrap;">
+<div style="width: 320px;"><a href="#"><img style="width: 320px; border-radius: 3%;" src="http://comm-tech.org/wp-content/uploads/2017/06/david_chabbi-225x300.png" /></a></div>
+<div class="colleagues-description" style="text-align: justify; flex: 1;">
+<div class="colleagues">David Chabbi</div>
+<div class="position">Trustee and adviser</div>
+
+<hr />
+<p style="max-width: 600px;">Building on university qualifications in technology and more than 10 years working professionally in IT, David joined as trustee and advisor in 2015. David is a specialist in online services provision.</p>
+
+</div>
+</div>
+</div>
